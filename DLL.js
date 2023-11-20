91deb64271c313ff0bb848d9441ee7d3d7487c79
Pravallika Yakkala <59355045+black-143@users.noreply.github.com>
2023-11-20 20:22:46 +0530

    Doubly linked list code

---
+++ DLL.js
@@ -0,0 +1,136 @@
+class Node{
+	constructor(data){
+		this.data=data
+		this.prev=null
+		this.next=null
+	}
+}
+
+class DoublyLinkedList{
+	constructor(){
+		this.head=null
+	}
+
+	addFirst(data){
+		const newNode = new Node(data)
+		newNode.next = this.head
+		if(this.head){
+			this.head.prev = newNode
+		}
+		this.head = newNode
+	}
+	addLast(data){
+		const newNode = new Node(data)
+		if(!this.head){
+			this.head  = newNode
+			return
+		}
+		let current = this.head
+		while(current.next){
+			current=current.next
+
+		}
+		newNode.prev=current
+		current.next = newNode
+	}
+	size(){
+		let count = 0
+		let current=this.head
+		while(current){
+			count++
+			current=current.next
+		}
+		return countu
+	}	
+	addAt(index,data){
+		if(index<0 || index > this.size()){
+			console.error("Invalid")
+		}
+		const newNode = new Node(data)
+		if(index == 0){
+			newNode.next = this.head
+			if(this.head){
+				this.head.prev=newNode
+			}
+			this.head=newNode
+		}
+		let current=this.head
+		for(let i=0;i<index;i++){
+			current=current.next
+		}
+		newNode.prev=current
+		newNode.next = current.next
+		if(current.next){
+			current.next.prev=newNode
+		}
+		current.next=newNode
+	}
+	removeFirst(){
+		if(!this.head) return
+
+		this.head=this.head.next
+
+		if(this.head){
+			this.head.prev=null
+		}
+	}
+	removeLast(){
+		if(!this.head){
+			return;
+		}
+		if(!this.head.next){
+			this.head=null
+			return;
+		}
+		let current=this.head
+		while(current.next.next){
+			current=current.next
+		}
+		current.next=null
+	}
+	removeAt(index){
+		if(index<0 || index > this.size()){
+			console.error("Invalid")
+		}
+		
+		if(index == 0){
+			
+		this.head=this.head.next
+
+		if(this.head){
+			this.head.prev=null
+		}
+			
+		}
+		let current=this.head
+		for(let i=0;i<index;i++){
+			current=current.next
+		}
+		
+		if(current.next){
+			current.next=current.next.next
+
+			if(current.next){
+				current.next.prev=current
+			}
+		}
+	
+	}
+
+	print(){
+		let current=this.head
+		while(current){
+			console.log(current.data)
+			current=current.next
+		}
+	}
+}
+
+const doulelinkedlist = new DoublyLinkedList();
+doulelinkedlist.addFirst(1)
+doulelinkedlist.addFirst(2)
+doulelinkedlist.addFirst(4)
+doulelinkedlist.addLast(8)
+doulelinkedlist.addAt(2,8)
+doulelinkedlist.removeFirst()
+doulelinkedlist.print()
